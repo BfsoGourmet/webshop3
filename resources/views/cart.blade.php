ad9fcd03a7e54f32e824dc345c5d58d52bea44fa
SimonLSchaller <simon.schaller701@gmail.com>
2023-03-22 14:22:25 +0100

    deleted span in cart

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -5,7 +5,6 @@
                 <div class="col-md-4 order-md-2 mb-4">
                     <h4 class="d-flex justify-content-between align-items-center mb-3">
                         <span class="text-muted">Ihr Warenkorb</span>
-                        <span class="badge badge-secondary badge-pill">3</span>
                     </h4>
                     <ul class="list-group mb-3">
                         @if ($products)
